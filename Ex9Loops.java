fe167f5d72d00d51e83c132c5a287f4cb29bff30
Lucas-san7os <lucasantos.cipriano@gmail.com>
2025-04-16 16:47:07 -0300

    Add files via upload
    Loop usando for 1

---
+++ Ex9Loops.java
@@ -0,0 +1,10 @@
+package ex9loops;
+public class Ex9Loops {
+
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++){
+            int resultado = 5 * i;
+            System.out.println("5X" + i+ "=" + resultado );
+        }
+    }
+}
