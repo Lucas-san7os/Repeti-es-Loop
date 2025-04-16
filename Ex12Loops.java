db52a9ae3a74642853503587fe136a34eec90350
Lucas-san7os <lucasantos.cipriano@gmail.com>
2025-04-16 16:50:39 -0300

    Add files via upload
    Loop usando for 4

---
+++ Ex12Loops.java
@@ -0,0 +1,11 @@
+package ex12loops;
+public class Ex12Loops {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i); // imprime em linhas separadas
+            }
+        }
+    }
+}
