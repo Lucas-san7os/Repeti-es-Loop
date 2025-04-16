5b27c94a259fd5eb07b11b0d2760ee1feec45718
Lucas-san7os <lucasantos.cipriano@gmail.com>
2025-04-16 16:39:38 -0300

    Add files via upload
    Loop usando while

---
+++ Ex1Loops.java
@@ -0,0 +1,14 @@
+package ex1loops;
+import java.util.Scanner;
+public class Ex1Loops {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner (System.in);
+        
+        int numero = 1;
+        while (numero <= 50){
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
